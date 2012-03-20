df9255ab7f9b81134dc4b5b4a53ba2a17394a2b7
sunrisela <sunrisela@gmail.com>
2012-03-20 16:49:44 +0800

    添加学生信息查看页面

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,14 @@
 $.metadata.setType("attr", "validation");
 
 $(function(){
-	$.post('menu_0.html',function(data){
-		$('#sidebar').append(data);
-	});
-	$("table").tablesorter({ 
+	$('#sidebar').load('menu_0.html');
+	$('.navbar.navbar-fixed-top').load('header.html');
+	
+	$("#list table").tablesorter({ 
 		sortList: [[1,0]],
 		headers: {
 			5: {sorter: false}
 		}
 	});
+	showMsg("测试show message");
 })
