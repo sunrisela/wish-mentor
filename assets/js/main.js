6ad446cd9455da54ce4377ad864de9e79497afaf
sunrisela <sunrisela@gmail.com>
2012-04-14 00:40:38 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,7 @@ $(function(){
 	});
 	showMsg("测试show message");
 	
+	// 初始化: 瀑布流布局
 	var $container = $('.thumbnails');
 	$container.imagesLoaded(function(){
 	  $container.masonry({
@@ -20,7 +21,12 @@ $(function(){
 	  });
 	});
 	
+	// 当点击学院tab的时候，触发该事件，
+	// reload瀑布流布局，防止因取不到image的width, height, 造成页面混乱
 	$('#college_select a[data-toggle="tab"]').on('shown', function (e) {
 		$($(this).attr("href")).find('.thumbnails').masonry('reload');
 	});
+	
+	// 初始化：详细资料 浮出框
+	$('a[rel="popover"]').popover();
 })
