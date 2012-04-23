b506b9006c5136c8a993dd1642d9ff83f33d7201
sunrisela <sunrisela@gmail.com>
2012-04-24 01:06:57 +0800

    加入日期控件

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,4 +59,15 @@ $(function(){
 		// TODO 发送请求
 		//$.post(url, params, function(){});
 	});
+	
+	// datepicker
+	$.datepicker.setDefaults({
+		dateFormat: "yy-mm-dd",
+		dayNames: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"],
+		dayNamesMin: ["日", "一", "二", "三", "四", "五", "六"],
+		monthNames: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+		nextText: "下一月",
+		prevText: "上一月"
+	});
+	$('#edit form #birth').datepicker();
 })
